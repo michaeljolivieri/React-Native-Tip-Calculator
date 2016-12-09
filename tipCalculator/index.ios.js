49666c3b14a13243ef38d7a6dd045bfc579ef35a
Zillabot <michael@howyoulove.com>
2016-12-09 14:12:29 -0800

    initialized react native project

---
+++ tipCalculator/index.ios.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class tipCalculator extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+         Amount Total:
+        </Text>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+});
+
+AppRegistry.registerComponent('tipCalculator', () => tipCalculator);
